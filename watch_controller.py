baee11ac11a67616f3f47609fd5429fe1f70bde1
Matt Martini <matt.martini@gmail.com>
2022-11-17 13:41:36 -0500

    check if controler process is running, email if not (crontab)

---
+++ watch_controller.py
@@ -0,0 +1,35 @@
+"""Watch processes to insure controller is running"""
+
+import psutil
+import mailer
+
+__author__ = "Matt Martini"
+__email__ = "matt.martini@imaginarywave.com"
+__version__ = "0.1.0"
+
+
+CONTROLLER_PID = "/run/controller.pid"
+
+
+def read_pid():
+    """Get the pid of the controller"""
+    pid_file = CONTROLLER_PID
+    with open(pid_file, "r", encoding="utf-8") as file:
+        return file.readline()
+
+
+def main():
+    """Check if controller is running, email alert if not"""
+    pid = int(read_pid())
+    pids = psutil.pids()
+
+    if (pid not in pids):
+        subject = "Greenhouse Controller DOWN!!"
+        message = "The Greenhouse Controller process is not running.  Restart!"
+        msg = mailer.Mailer(subject, message)
+        msg.send()
+        msg.cleanup()
+
+
+if __name__ == "__main__":
+    main()
