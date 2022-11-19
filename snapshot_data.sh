1dec31c73814260b91b7162150aa908d634abc37
Matt Martini <matt.martini@gmail.com>
2022-11-19 11:23:38 -0500

    convert to daemon_tools from daemonize

--- snapshot_data.sh
+++ snapshot_data.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Getting Temperature Data. Please wait 30 seconds."
-kill -1 $(cat /run/controller.pid)
+
+sudo svc -h /service/greenhouse
 sleep 30
-tail -n 13 controller_*.out
+tail -n 13 /var/log/greenhouse/current | tai64nlocal | cut -d" " -f 3-
--- watch_controller.py
+++ watch_controller.py
@@ -1,6 +1,7 @@
 """Watch processes to insure controller is running"""
 
-import psutil
+import subprocess
+
 import mailer
 
 __author__ = "Matt Martini"
@@ -8,22 +9,14 @@ __email__ = "matt.martini@imaginarywave.com"
 __version__ = "0.1.0"
 
 
-CONTROLLER_PID = "/run/controller.pid"
-
-
-def read_pid():
-    """Get the pid of the controller"""
-    pid_file = CONTROLLER_PID
-    with open(pid_file, "r", encoding="utf-8") as file:
-        return file.readline()
+CMD = "/usr/local/bin/svok"
+ARGS = "/service/greenhouse"
 
 
 def main():
     """Check if controller is running, email alert if not"""
-    pid = int(read_pid())
-    pids = psutil.pids()
 
-    if (pid not in pids):
+    if subprocess.run([CMD, ARGS]).returncode:
         subject = "Greenhouse Controller DOWN!!"
         message = "The Greenhouse Controller process is not running.  Restart!"
         msg = mailer.Mailer(subject, message)
