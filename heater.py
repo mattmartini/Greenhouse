66b587bd584536559a4ecc2483d49ac951e2c630
Matt Martini <matt.martini@gmail.com>
2022-12-02 15:13:59 -0500

    conversion for dual power heater

--- heater.py
+++ heater.py
@@ -12,13 +12,15 @@ __version__ = "1.0.0"
 class Heater:
     """Heater with variable power output aproximated via time slicing."""
 
-    def __init__(self, pin=21):
+    def __init__(self, pin=21, sw_pin=20):
         """Initialize heater."""
         self.pin = pin
+        self.sw_pin = sw_pin
 
         # GPIO Setup
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.setup(self.sw_pin, GPIO.OUT)
 
     def turn_on(self):
         """Turn the heater on."""
@@ -28,10 +30,35 @@ class Heater:
         """Turn the heater off."""
         GPIO.output(self.pin, GPIO.LOW)
 
-    def heater_cycle(self, power, cycle_time=10):
-        """Use percentage of time cycle to aproximate variable power."""
-        if (power < 0) or (power > 100):
+    def mode_half(self):
+        """Switch the heater to Half Power mode."""
+        GPIO.output(self.sw_pin, GPIO.LOW)
+        # TODO check polarity
+
+    def mode_full(self):
+        """Turn the heater to Full Power mode."""
+        GPIO.output(self.sw_pin, GPIO.HIGH)
+
+    def heater_cycle(self, power, cycle_time=25):
+        """Use percentage of time cycle to aproximate variable power.
+        New heater has two power modes Half (750W) and Full (1500W)
+        In order to have a more natural response we use a second relay
+        to control which mode the heat utilizes. For computed power
+        less than 50%, Half power mode for twice the duration and
+        more than 50%, Full power mode for normal duration.
+        So Half power for 100% cycle is the transition to Full power
+        for 50% cycle.
+        """
+        if power < 0:
             power = 0
+        elif power > 100:
+            power = 100
+
+        if power < 50:
+            self.mode_half()
+            power = power * 2
+        else:
+            self.mode_full()
 
         on_time = cycle_time * power / 100
         off_time = cycle_time * (100 - power) / 100
@@ -46,6 +73,7 @@ class Heater:
     def cleanup(self):
         """Close resources."""
         self.turn_off()
+        self.mode_half()
         GPIO.cleanup()
 
 
@@ -54,14 +82,15 @@ def test():
 
     my_heater = Heater()
 
-    for _ in range(3):
-        my_heater.turn_on()
-        time.sleep(5.0)
-        my_heater.turn_off()
-        time.sleep(2.0)
+    # for _ in range(3):
+    #     my_heater.turn_on()
+    #     time.sleep(5.0)
+    #     my_heater.turn_off()
+    #     time.sleep(2.0)
 
-    my_heater.heater_cycle(20, cycle_time=15)
-    my_heater.heater_cycle(80)
+    my_heater.heater_cycle(30, cycle_time=30)
+    my_heater.heater_cycle(60, cycle_time=30)
+    my_heater.heater_cycle(20, cycle_time=30)
 
     my_heater.cleanup()
 
