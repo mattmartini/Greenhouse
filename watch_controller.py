836017c52fd33d85c25670b6dd19c6238268f1e0
Matt Martini <matt.martini@gmail.com>
2024-11-17 14:50:30 -0500

    refactor(subprocess): Explicitly state check=False

--- watch_controller.py
+++ watch_controller.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 """Watch processes to insure controller is running"""
 
 import subprocess
 
+# import psutil
 import mailer
 
 __author__ = "Matt Martini"
@@ -12,11 +14,23 @@ __version__ = "0.1.0"
 CMD = "/usr/local/bin/svok"
 ARGS = "/service/greenhouse"
 
+CONTROLLER_PID = "/run/controller.pid"
+
+
+# def read_pid():
+#     """Get the pid of the controller"""
+#     pid_file = CONTROLLER_PID
+#     with open(pid_file, "r", encoding="utf-8") as file:
+#         return file.readline()
+
 
 def main():
     """Check if controller is running, email alert if not"""
+    # pid = int(read_pid())
+    # pids = psutil.pids()
 
-    if subprocess.run([CMD, ARGS]).returncode:
+    # if (pid not in pids):
+    if subprocess.run([CMD, ARGS], check=False).returncode:
         subject = "Greenhouse Controller DOWN!!"
         message = "The Greenhouse Controller process is not running.  Restart!"
         msg = mailer.Mailer(subject, message)
